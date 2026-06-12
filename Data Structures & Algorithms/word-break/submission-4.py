d1c16e70e0edeb568dedfa15a97ce2c740cd2789
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-06-11 21:11:57 -0400

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # RECURSIVE SOLUTION --> O(n * t) space
+        wordHash = {wordDict[i] for i in range(len(wordDict))}
+        max_len = -float('inf')
+        for word in wordDict:
+            if len(word) >= max_len:
+                max_len = len(word)
+        
+        memo = {}
+        def breakWords(i, growing_word):
+            # base case
+            if len(growing_word) > max_len:
+                return False
+            if i == len(s) - 1:
+                return growing_word + s[i] in wordHash
+            # recursive case
+            if (i, growing_word) in memo:
+                return memo[(i, growing_word)]
+            can_segment = False
+            if growing_word + s[i] in wordHash:
+                can_segment = can_segment or breakWords(i + 1, "")
+            can_segment = can_segment or breakWords(i + 1, growing_word + s[i])
+            memo[(i, growing_word)] = can_segment
+            return can_segment
+        return breakWords(0, "")
+        
+            
