510f42f2232ca428ab0eb584495236693fcfeda9
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-06-12 23:45:34 -0400

    Add: non-cyclical-number - submission-6

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        visited = set()
+        def is_cycle(num):
+            if num in visited:
+                return False
+            if num == 1:
+                return True
+            sum_of_squares = 0
+            for char_num in str(num):
+                sum_of_squares += int(char_num)**2 
+            visited.add(num)
+            return is_cycle(sum_of_squares)
+
+
+        return is_cycle(n)
+        
