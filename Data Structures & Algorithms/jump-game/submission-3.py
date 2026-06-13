26ad5e92fc3063dbec836b88c2c2339c95040919
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-06-12 20:59:15 -0400

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # ITERATIVE SOLUTION --> O(n) time
+        
+        # Initialization
+        j = len(nums) - 1
+
+        # Maintenance
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] + i >= j:
+                j = i
+            else:
+                continue
+        if j == 0:
+            return True
+        else:
+            return False
+
+
