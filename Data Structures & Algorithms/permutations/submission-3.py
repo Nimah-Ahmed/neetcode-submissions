4ac9d4e1192004e975722b4c01800fecf095785b
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-06-19 20:34:14 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        Algorithm:
+            S (subproblem): M(i, taken) represents all possible
+            permutations starting with i
+            R (relate):
+
+            concatenate all possibilities
+            M (i, taken) = For any j such that nums[j] is not in taken
+                                M(j, taken + {nums[j]})
+            
+            T (topological order): taken always increases in size
+            B (base case) = taken == set(nums). There is nothing left
+            be taken. We return the path. 
+            O (output) = M(0, taken = {})
+            T (runtime) = O(n * n!), space = O(n)
+        """
+        result = []
+        path = []
+
+        def recurse(taken):
+            # base case
+            if len(taken) == len(nums):
+                result.append(path.copy())
+                return result
+            # recursive case
+            for j in range(len(nums)):
+                if nums[j] not in taken:
+                    path.append(nums[j])
+                    taken.add(nums[j])
+                    recurse(taken)
+                    taken.remove(nums[j])
+                    path.pop()
+        recurse( set())
+        return result
+
+
+                
+
