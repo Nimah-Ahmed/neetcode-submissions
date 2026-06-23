75bb3355f59ba745cb67178247a4278bae162594
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-06-23 16:14:53 -0400

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # 1. Create the graph
+        graph = {}
+        for vertex_1, vertex_2 in edges:
+            if vertex_1 not in graph:
+                graph[vertex_1] = {vertex_2}
+            else:
+                graph[vertex_1].add(vertex_2)
+            if vertex_2 not in graph:
+                graph[vertex_2] = {vertex_1}
+            else:
+                graph[vertex_2].add(vertex_1)
+        
+        for i in range(n):
+            if i not in graph:
+                graph[i] = set()
+
+        # 2. DFS code
+        visited = set()
+        def dfs(node):
+            visited.add(node)
+            for next_node in graph[node]:
+                if next_node not in visited:
+                    dfs(next_node)
+        
+        # 3. Full-DFS on every node
+        connected_components = 0
+        for node in graph:
+            if node not in visited:
+                connected_components += 1
+                dfs(node)
+        
+        return connected_components
+
