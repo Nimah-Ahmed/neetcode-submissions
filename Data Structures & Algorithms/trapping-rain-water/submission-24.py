726ca48f9fd80c5a8069c72a6cb9e7c921996e1f
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-07-07 20:27:26 -0400

    Add: trapping-rain-water - submission-24

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-24.py
@@ -0,0 +1,71 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # 1. First identify starting blocks capable of trapping water
+        # 2a. Initialization
+        stack = []
+        start_of_container = {}
+        # 2b. Maintenance
+        for i in range(len(height)):
+            if height[i] == 0:
+                continue
+            if stack == []:
+                stack.append((height[i], i))
+                continue
+            else:
+                last_element = None
+                while stack != []:
+                    if stack[-1][0] > height[i]:
+                        start_of_container[stack[-1][1]] = i
+                        break
+                    elif stack[-1][0] == height[i]:
+                        start_of_container[stack[-1][1]] = i
+                        stack.pop()
+                        break
+                    elif stack[-1][0] < height[i]:
+                        if len(stack) == 1:
+                            last_element = stack.pop()
+                            break
+                        stack.pop()
+                if last_element != None:
+                    start_of_container[last_element[1]] = i
+                stack.append((height[i], i))
+        # 2c. Termination: None
+        print(start_of_container)
+
+        # 3. Count Water
+        total = 0
+        i = 0
+        while i < len(height):
+            print("i", i)
+            if i in start_of_container:
+                start = i
+                end = start_of_container[start] # inclusive
+                cap = min(height[start], height[end])
+                print("cap", cap)
+                if abs(start - end) <= 1:
+                    i = end
+                    continue
+                else:
+                    current_bucket = 0
+                    for j in range(start, end + 1):
+                        print("heightj", height[j])
+                        print(cap - height[j])
+                        if cap - height[j] < 0:
+                            continue
+                        current_bucket += cap - height[j]
+                    print("current bucket", current_bucket)
+                    total += current_bucket
+                i = end
+            else:
+                i += 1
+        return total
+
+
+
+
+       
+                
+
+
+
+        
