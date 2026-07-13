3e3fdc106041059542223018d0a784ea7a2671d3
Nimah Ahmed <148268308+Nimah-Ahmed@users.noreply.github.com>
2026-07-12 22:06:08 -0400

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        pointer_1 = 0
+        pointer_2 = 0
+        output = ""
+        while pointer_1 < len(word1) and pointer_2 < len(word2):
+            output += word1[pointer_1] + word2[pointer_2]
+            pointer_1 += 1
+            pointer_2 += 1
+        if pointer_1 < len(word1):
+            while pointer_1 < len(word1):
+                output += word1[pointer_1]
+                pointer_1 += 1
+            return output
+        if pointer_2 < len(word2):
+            while pointer_2 < len(word2):
+                output += word2[pointer_2]
+                pointer_2 += 1
+            return output
+        return output
